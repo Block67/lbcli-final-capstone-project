a9fd0e91b09a4a6d285ac9bfb1ed4b6eefbe39e2
boda <block6703@gmail.com>
2026-04-23 01:30:22 +0100

    chore: lbcli-final-capstone-project

--- submission/05.sh
+++ submission/05.sh
@@ -1,1 +1,10 @@
 # How many satoshis did this transaction pay for fee?: b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+TX=b71fb9ab7707407cc7265591e0c0d47d07afede654f91de1f63c0cb522914bcb
+TX_DATA=$(bitcoin-cli -signet getrawtransaction $TX true)
+OUT_SUM=$(echo "$TX_DATA" | jq '[.vout[].value] | add * 100000000 | round')
+IN_SATS=0
+while IFS=' ' read -r txid vout; do
+  VAL=$(bitcoin-cli -signet getrawtransaction "$txid" true | jq -r ".vout[$vout].value * 100000000 | round")
+  IN_SATS=$((IN_SATS + VAL))
+done <<< "$(echo "$TX_DATA" | jq -r '.vin[] | "\(.txid) \(.vout)"')"
+echo $((IN_SATS - OUT_SUM))
--- submission/07.sh
+++ submission/07.sh
@@ -1,1 +1,2 @@
 # what is the coinbase tx in this block 243,834
+bitcoin-cli -signet getblockhash 243834 | xargs bitcoin-cli -signet getblock | jq -r '.tx[0]'
